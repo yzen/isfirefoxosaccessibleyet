58ba4167e0e63c79b5faf1afdc2810e2a691b982
Yura Zenevich <yzenevich@mozilla.com>
2014-07-03 01:35:12 -0400

    more visual tweaks

--- js/main.js
+++ js/main.js
@@ -2,4 +2,23 @@
 
 (function() {
 
+  function get(path, callback) {
+    var req =  new XMLHttpRequest();
+    req.onload = callback;
+    req.open('GET', path, true);
+    req.send();
+  }
+
+  get('https://api-dev.bugzilla.mozilla.org/latest/bug?' +
+    'whiteboard=[b2ga11y p=1]&' +
+    'status=NEW&' +
+    'status=REOPENED&' +
+    'component=Gaia::System&' +
+    'component=Gaia::System::Lockscreen&' +
+    'component=Gaia::System::Input Mgmt&' +
+    'component=Gaia::System::Browser Chrome&' +
+    'component=Gaia::System::Window Mgmt', function() {
+      console.log(this.responseText);
+    });
+
 })();
